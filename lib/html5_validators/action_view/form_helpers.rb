69b5eb1c1b54f851394253f4b2b4e3a103cc87e0
Markus Doits <markus.doits@googlemail.com>
2014-02-04 19:33:34 +0100

    reindent, remove rails < 4 support, update version

--- lib/html5_validators/action_view/form_helpers.rb
+++ lib/html5_validators/action_view/form_helpers.rb
@@ -1,133 +1,91 @@
 module Html5Validators
-  module ActionViewExtension
-    def validation_active?
-      object.class.ancestors.include?(ActiveModel::Validations) && (object.auto_html5_validation != false) && (object.class.auto_html5_validation != false)
-    end
+    module ActionViewExtension
+        def validation_active?
+            object.class.ancestors.include?(ActiveModel::Validations) && (object.auto_html5_validation != false) && (object.class.auto_html5_validation != false)
+        end
 
-    def inject_required_field
-        @options["required"] ||= object.attribute_required?(@method_name) if validation_active?
-    end
+        def inject_required_field
+            @options["required"] ||= object.attribute_required?(@method_name) if validation_active?
+        end
 
-    def inject_readonly_field
-        @options["readonly"] ||= object.attribute_readonly?(@method_name) if validation_active?
-    end
+        def inject_readonly_field
+            @options["readonly"] ||= object.attribute_readonly?(@method_name) if validation_active?
+        end
 
-    def inject_maxlength_field
-        @options["maxlength"] ||= object.attribute_maxlength(@method_name) if validation_active?
-    end
+        def inject_maxlength_field
+            @options["maxlength"] ||= object.attribute_maxlength(@method_name) if validation_active?
+        end
 
-    def inject_minlength_field
-        @options["data-minlength"] ||= object.attribute_minlength(@method_name) if validation_active?
-    end
+        def inject_minlength_field
+            @options["data-minlength"] ||= object.attribute_minlength(@method_name) if validation_active?
+        end
 
-    def inject_dependent_validation
-        @options["data-dependent-validation"] ||= object.attribute_dependent_validation(@method_name) if validation_active?
-    end
+        def inject_dependent_validation
+            @options["data-dependent-validation"] ||= object.attribute_dependent_validation(@method_name) if validation_active?
+        end
 
-    def inject_min_max
-        return unless validation_active?
-        @options["max"] ||= object.attribute_max(@method_name)
-        @options["min"] ||= object.attribute_min(@method_name)
+        def inject_min_max
+            return unless validation_active?
+            @options["max"] ||= object.attribute_max(@method_name)
+            @options["min"] ||= object.attribute_min(@method_name)
+        end
     end
-  end
-end if ActionPack::VERSION::STRING >= '4'
-
+end
 
 module ActionView
-  module Helpers
-    module FormHelper
-      def form_for_with_auto_html5_validation_option(record, options = {}, &proc)
-        record.auto_html5_validation = false if (options[:auto_html5_validation] == false) && (record.respond_to? :auto_html5_validation=)
-        form_for_without_auto_html5_validation_option record, options, &proc
-      end
-      alias_method_chain :form_for, :auto_html5_validation_option
-    end
-
-    if ActionPack::VERSION::STRING >= '4'
-      module Tags
-        class Base #:nodoc:
-          include Html5Validators::ActionViewExtension
-        end
-
-        class TextField
-          def render_with_html5_attributes
-            inject_required_field
-            inject_readonly_field
-            inject_maxlength_field
-            inject_dependent_validation
-            inject_minlength_field
-            inject_min_max
-
-            render_without_html5_attributes
-          end
-          alias_method_chain :render, :html5_attributes
-        end
-
-        class TextArea
-          def render_with_html5_attributes
-            inject_required_field
-            inject_readonly_field
-            inject_maxlength_field
-            inject_minlength_field
-            inject_min_max
-
-            render_without_html5_attributes
-          end
-          alias_method_chain :render, :html5_attributes
-        end
-
-        #TODO probably I have to add some more classes here
-        [RadioButton, CheckBox, Select, DateSelect, TimeZoneSelect].each do |kls|
-          kls.class_eval do
-            def render_with_html5_attributes
-              inject_required_field
-              inject_readonly_field
-
-              render_without_html5_attributes
+    module Helpers
+        module FormHelper
+            def form_for_with_auto_html5_validation_option(record, options = {}, &proc)
+                record.auto_html5_validation = false if (options[:auto_html5_validation] == false) && (record.respond_to? :auto_html5_validation=)
+                form_for_without_auto_html5_validation_option record, options, &proc
             end
-            alias_method_chain :render, :html5_attributes
-          end
+            alias_method_chain :form_for, :auto_html5_validation_option
         end
-      end
-    # ActionPack::VERSION::STRING == '3'
-    else
-      class InstanceTag
-        def to_input_field_tag_with_html5_attributes(field_type, options = {})
-          if object.class.ancestors.include?(ActiveModel::Validations) && (object.auto_html5_validation != false) && (object.class.auto_html5_validation != false)
-            options["required"] ||= object.attribute_required?(method_name)
-            options["maxlength"] ||= object.attribute_maxlength(method_name)
-            options["max"] ||= object.attribute_max(method_name)
-            options["min"] ||= object.attribute_min(method_name)
-          end
-          to_input_field_tag_without_html5_attributes field_type, options
-        end
-        alias_method_chain :to_input_field_tag, :html5_attributes
 
-        def to_text_area_tag_with_html5_attributes(options = {})
-          if object.class.ancestors.include?(ActiveModel::Validations) && (object.auto_html5_validation != false) && (object.class.auto_html5_validation != false)
-            options["required"] ||= object.attribute_required?(method_name)
-            options["maxlength"] ||= object.attribute_maxlength(method_name)
-          end
-          to_text_area_tag_without_html5_attributes options
-        end
-        alias_method_chain :to_text_area_tag, :html5_attributes
+        module Tags
+            class Base #:nodoc:
+                include Html5Validators::ActionViewExtension
+            end
 
-        def to_radio_button_tag_with_html5_attributes(tag_value, options = {})
-          if object.class.ancestors.include?(ActiveModel::Validations) && (object.auto_html5_validation != false) && (object.class.auto_html5_validation != false)
-            options["required"] ||= object.attribute_required?(method_name)
-          end
-          to_radio_button_tag_without_html5_attributes tag_value, options
-        end
-        alias_method_chain :to_radio_button_tag, :html5_attributes
+            class TextField
+                def render_with_html5_attributes
+                    inject_required_field
+                    inject_readonly_field
+                    inject_maxlength_field
+                    inject_dependent_validation
+                    inject_minlength_field
+                    inject_min_max
 
-        def to_check_box_tag_with_html5_attributes(options = {}, checked_value = "1", unchecked_value = "0")
-          if object.class.ancestors.include?(ActiveModel::Validations) && (object.auto_html5_validation != false) && (object.class.auto_html5_validation != false)
-            options["required"] ||= object.attribute_required?(method_name)
-          end
-          to_check_box_tag_without_html5_attributes options, checked_value, unchecked_value
+                    render_without_html5_attributes
+                end
+                alias_method_chain :render, :html5_attributes
+            end
+
+            class TextArea
+                def render_with_html5_attributes
+                    inject_required_field
+                    inject_readonly_field
+                    inject_maxlength_field
+                    inject_minlength_field
+                    inject_min_max
+
+                    render_without_html5_attributes
+                end
+                alias_method_chain :render, :html5_attributes
+            end
+
+            #TODO probably I have to add some more classes here
+            [RadioButton, CheckBox, Select, DateSelect, TimeZoneSelect].each do |kls|
+                kls.class_eval do
+                    def render_with_html5_attributes
+                        inject_required_field
+                        inject_readonly_field
+
+                        render_without_html5_attributes
+                    end
+                    alias_method_chain :render, :html5_attributes
+                end
+            end
         end
-        alias_method_chain :to_check_box_tag, :html5_attributes
-      end
     end
-  end
 end
--- lib/html5_validators/active_model/helper_methods.rb
+++ lib/html5_validators/active_model/helper_methods.rb
@@ -1,65 +1,65 @@
 module ActiveModel
-  module Validations
-    module HelperMethods
-      def current_validators
-          self.respond_to?(:validators) ? self.validators : self.class.validators
-      end
+    module Validations
+        module HelperMethods
+            def current_validators
+                self.respond_to?(:validators) ? self.validators : self.class.validators
+            end
 
-      def current_object
-          self.respond_to?(:validators) ? nil : self
-      end
+            def current_object
+                self.respond_to?(:validators) ? nil : self
+            end
 
-      def validator_match?(validator)
-          return true if !validator.options.include?(:on) || !current_object
+            def validator_match?(validator)
+                return true if !validator.options.include?(:on) || !current_object
 
-          case validator.options[:on]
-          when :save
-              true
-          when :create
-              !current_object.persisted?
-          when :update
-              current_object.persisted?
-          end
-      end
+                case validator.options[:on]
+                when :save
+                    true
+                when :create
+                    !current_object.persisted?
+                when :update
+                    current_object.persisted?
+                end
+            end
 
-      def attribute_required?(attribute)
-        current_validators.grep(PresenceValidator).any? do |v|
-          validator_match?(v) && v.attributes.include?(attribute.to_sym) && (v.options.keys & [:if, :unless]).empty?
-        end ||
-        attribute.to_s.end_with?('_confirmation') && self.attribute_required?(attribute.to_s.sub(/_confirmation$/, '').to_sym) # hacky but works?
-      end
+            def attribute_required?(attribute)
+                current_validators.grep(PresenceValidator).any? do |v|
+                    validator_match?(v) && v.attributes.include?(attribute.to_sym) && (v.options.keys & [:if, :unless]).empty?
+                end ||
+                attribute.to_s.end_with?('_confirmation') && self.attribute_required?(attribute.to_s.sub(/_confirmation$/, '').to_sym) # hacky but works?
+            end
 
-      def attribute_readonly?(attribute)
-          !current_object || !current_object.new_record? && current_object.class._attr_readonly.include?(attribute.to_s)
-      end
+            def attribute_readonly?(attribute)
+                !current_object || !current_object.new_record? && current_object.class._attr_readonly.include?(attribute.to_s)
+            end
 
-      def attribute_maxlength(attribute)
-        current_validators.grep(LengthValidator).select {|v|
-          validator_match?(v) && v.attributes.include?(attribute.to_sym) && (v.options.keys & [:maximum, :is]).any? && (v.options.keys & [:if, :unless]).empty?
-        }.map {|v| v.options.slice(:maximum, :is)}.map(&:values).flatten.max
-      end
+            def attribute_maxlength(attribute)
+                current_validators.grep(LengthValidator).select {|v|
+                    validator_match?(v) && v.attributes.include?(attribute.to_sym) && (v.options.keys & [:maximum, :is]).any? && (v.options.keys & [:if, :unless]).empty?
+                }.map {|v| v.options.slice(:maximum, :is)}.map(&:values).flatten.max
+            end
 
-      def attribute_minlength(attribute)
-        current_validators.grep(LengthValidator).select {|v|
-          validator_match?(v) && v.attributes.include?(attribute.to_sym) && (v.options.keys & [:minimum, :is]).any? && (v.options.keys & [:if, :unless]).empty?
-        }.map {|v| v.options.slice(:minimum, :is)}.map(&:values).flatten.min
-      end
+            def attribute_minlength(attribute)
+                current_validators.grep(LengthValidator).select {|v|
+                    validator_match?(v) && v.attributes.include?(attribute.to_sym) && (v.options.keys & [:minimum, :is]).any? && (v.options.keys & [:if, :unless]).empty?
+                }.map {|v| v.options.slice(:minimum, :is)}.map(&:values).flatten.min
+            end
 
-      def attribute_max(attribute)
-        current_validators.grep(NumericalityValidator).select {|v|
-          validator_match?(v) && v.attributes.include?(attribute.to_sym) && (v.options.keys & [:less_than, :less_than_or_equal_to]).any? && (v.options.keys & [:if, :unless]).empty?
-        }.map {|v| v.options.slice(:less_than, :less_than_or_equal_to)}.map(&:values).flatten.max
-      end
+            def attribute_max(attribute)
+                current_validators.grep(NumericalityValidator).select {|v|
+                    validator_match?(v) && v.attributes.include?(attribute.to_sym) && (v.options.keys & [:less_than, :less_than_or_equal_to]).any? && (v.options.keys & [:if, :unless]).empty?
+                }.map {|v| v.options.slice(:less_than, :less_than_or_equal_to)}.map(&:values).flatten.max
+            end
 
-      def attribute_min(attribute)
-        current_validators.grep(NumericalityValidator).select {|v|
-          validator_match?(v) && v.attributes.include?(attribute.to_sym) && (v.options.keys & [:greater_than, :greater_than_or_equal_to]).any? && (v.options.keys & [:if, :unless]).empty?
-        }.map {|v| v.options.slice(:greater_than, :greater_than_or_equal_to)}.map(&:values).flatten.min
-      end
+            def attribute_min(attribute)
+                current_validators.grep(NumericalityValidator).select {|v|
+                    validator_match?(v) && v.attributes.include?(attribute.to_sym) && (v.options.keys & [:greater_than, :greater_than_or_equal_to]).any? && (v.options.keys & [:if, :unless]).empty?
+                }.map {|v| v.options.slice(:greater_than, :greater_than_or_equal_to)}.map(&:values).flatten.min
+            end
 
-      def attribute_dependent_validation(attribute)
-        "#{current_object.class.to_s.underscore}_#{attribute.to_s.sub(/_confirmation$/, '')}" if current_object && attribute.to_s.end_with?('_confirmation')
-      end
+            def attribute_dependent_validation(attribute)
+                "#{current_object.class.to_s.underscore}_#{attribute.to_s.sub(/_confirmation$/, '')}" if current_object && attribute.to_s.end_with?('_confirmation')
+            end
+        end
     end
-  end
 end
